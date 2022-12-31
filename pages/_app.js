8fe0f9f2890e13f1644b7f92284cb861c1f6ab41
wardy30 <hairul.wardy@digitalent.id>
2023-01-01 00:33:43 +0800

    add component form

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "../styles/globals.css";
+import ContextProvider from "../src/context";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ContextProvider>
+      <Component {...pageProps} />
+    </ContextProvider>
+  );
 }
--- pages/verif-email/index.js
+++ pages/verif-email/index.js
@@ -1,9 +1,17 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import { HiChatAlt2 } from "react-icons/hi";
+import OtpInput from "react-otp-input";
+
+import GreetingAuth from "../../src/components/GreetingAuth";
+import Layout from "../../src/components/Layout";
+import { AuthContext } from "../../src/context/providers/AuthProvider";
 import usePost from "../../src/hooks/usePost";
 
 const VerifEmail = () => {
-  const [email, setEmail] = useState("");
+  // const [email, setEmail] = useState("");
+  const { email, setEmail } = useContext(AuthContext);
   const [otp, setOtp] = useState("");
+  const [state, setState] = useState("");
 
   const data = {
     email,
@@ -11,44 +19,83 @@ const VerifEmail = () => {
   };
   const { handlePostData } = usePost("verify_otp", data, "/register");
 
+  const handleChange = (state) => {
+    setState(state);
+  };
+
   return (
-    <form
-      className="container"
-      style={{ width: 350 }}
-      onSubmit={(e) => handlePostData(e)}
-    >
-      <div className="mb-3">
-        <label htmlFor="exampleInputEmail1" className="form-label">
-          Masukkan Email
-        </label>
-        <input
-          type="email"
-          className="form-control"
-          id="exampleInputEmail1"
-          aria-describedby="emailHelp"
-          required
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
+    <Layout>
+      <div style={{ marginTop: -100 }}>
+        <h1>adsfasdf{state.otp}</h1>
+        <GreetingAuth
+          title="Hallo, Silahkan ikuti proses registrasi👋"
+          icon={<HiChatAlt2 color="#14BA6D" size={46} />}
         />
+        <form
+          className="card p-3"
+          style={{ width: 350 }}
+          onSubmit={(e) => handlePostData(e)}
+        >
+          <div className="text-center fw-medium">
+            <p className="opacity-75">
+              Silahkan cek email{" "}
+              <span style={{ color: "#14BA6D" }}>sdf@gmail.com</span> untuk kode
+              OTP yang telah kami kirim.
+            </p>
+          </div>
+          <div className="mb-3">
+            <OtpInput
+              numInputs={6}
+              // containerStyle={{ backgroundColor: "red" }}
+              inputStyle={{
+                width: "100%",
+                padding: "0.7rem",
+                border: "1px solid #C8C8C8",
+                borderRadius: "8px",
+                marginRight: 6,
+                color: "#14BA6D",
+                fontWeight: "bold",
+              }}
+              isInputNum={true}
+              shouldAutoFocus={true}
+              value={state}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputEmail1" className="form-label">
+              Masukkan Email
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="otp" className="form-label">
+              Masukkan OTP
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="otp"
+              aria-describedby="otp"
+              required
+              value={otp}
+              onChange={(e) => setOtp(e.target.value)}
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
       </div>
-      <div className="mb-3">
-        <label htmlFor="otp" className="form-label">
-          Masukkan OTP
-        </label>
-        <input
-          type="text"
-          className="form-control"
-          id="otp"
-          aria-describedby="otp"
-          required
-          value={otp}
-          onChange={(e) => setOtp(e.target.value)}
-        />
-      </div>
-      <button type="submit" className="btn btn-primary">
-        Submit
-      </button>
-    </form>
+    </Layout>
   );
 };
 
