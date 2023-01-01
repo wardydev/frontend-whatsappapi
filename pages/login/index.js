6672d79114a5521b1f38abf3743e5615a1bb756d
wardy30 <hairul.wardy@digitalent.id>
2023-01-01 09:17:23 +0800

    fix login page component

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,4 +1,9 @@
 import React, { useState } from "react";
+import { HiChatAlt2 } from "react-icons/hi";
+import Button from "../../src/components/Button";
+import GreetingAuth from "../../src/components/GreetingAuth";
+import InputField from "../../src/components/InputField";
+import Layout from "../../src/components/Layout";
 import usePost from "../../src/hooks/usePost";
 
 const Login = () => {
@@ -18,45 +23,46 @@ const Login = () => {
   };
   const { handlePostData } = usePost("login", data, "/", true, "TOKEN");
   return (
-    <form
-      className="container"
-      style={{ width: 350 }}
-      onSubmit={(e) => handlePostData(e)}
-    >
-      <div className="mb-3">
-        <label htmlFor="masukkanEmail" className="form-label">
-          Masukkan Email
-        </label>
-        <input
-          type="email"
-          className="form-control"
-          id="masukkanEmail"
-          aria-describedby="emailHelp"
-          required
-          name="email"
-          value={value.email}
-          onChange={(e) => handleInputchange(e)}
+    <Layout>
+      <div style={{ marginTop: -100 }}>
+        <h1></h1>
+        <GreetingAuth
+          title="Yuk, Daftar dulu disini👋"
+          icon={<HiChatAlt2 color="#14BA6D" size={46} />}
         />
+        <form
+          className="container"
+          style={{ width: 350 }}
+          onSubmit={(e) => handlePostData(e)}
+        >
+          <div className="mb-1">
+            <InputField
+              label="Email"
+              type="email"
+              placeholder="Tambahkan Email"
+              id="exampleInputEmail1"
+              isRequired={true}
+              name="email"
+              value={value.email}
+              setValue={(e) => handleInputchange(e)}
+            />
+          </div>
+          <div className="mb-3">
+            <InputField
+              label="Password"
+              type="password"
+              placeholder="Tambahkan Password"
+              id="password"
+              isRequired={true}
+              name="password"
+              value={value.password}
+              setValue={(e) => handleInputchange(e)}
+            />
+          </div>
+          <Button title="Login" />
+        </form>
       </div>
-      <div className="mb-3">
-        <label htmlFor="masukkanPassword" className="form-label">
-          Password
-        </label>
-        <input
-          type="password"
-          className="form-control"
-          id="masukkanPassword"
-          aria-describedby="password"
-          required
-          name="password"
-          value={value.password}
-          onChange={(e) => handleInputchange(e)}
-        />
-      </div>
-      <button type="submit" className="btn btn-primary">
-        Submit
-      </button>
-    </form>
+    </Layout>
   );
 };
 
